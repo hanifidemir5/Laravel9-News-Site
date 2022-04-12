6589158648ff96966cda7a0f4cef5f60da049ddb
Hanifi Demir <1910213059@ogrenci.karabuk.edu.tr>
2022-04-12 10:14:11 +0300

    Migration_Update

---
+++ resources/views/admin/Category/create.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.AdminBase')
+
+@section('title', 'Category Create')
+
+@section('content')
+    <!-- ============================================================== -->
+    <!-- wrapper  -->
+    <!-- ============================================================== -->
+    <div class="dashboard-wrapper">
+        <div class="dashboard-ecommerce">
+            <div class="container-fluid dashboard-content ">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title">Add Category</h2>
+                            <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end pageheader  -->
+                <!-- ============================================================== -->
+
+                <div class="card">
+                    <h5 class="card-header">Category Input</h5>
+                    <div class="card-body">
+                        <form role="form" action="/admin/category/store" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="inputText3" class="col-form-label">Title</label>
+                                <input type="text" class="form-control" name="title" placeholder="Title">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputText3" class="col-form-label">Keywords</label>
+                                <input type="text" class="form-control" name="keywords" placeholder="Keywords">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputText3" class="col-form-label">Description</label>
+                                <input type="text" class="form-control" name="description" placeholder="Description">
+                            </div>
+                            <label for="customFile">Image</label>
+                            <div class="custom-file mb-3">
+                                <input type="file" class="custom-file-input" name="image">
+                                <label class="custom-file-label" for="customFile">Image</label>
+                            </div>
+                            <label>Status</label>
+                            <select class="form-control" id="input-select">
+                                <option>True</option>
+                                <option>False</option>
+                            </select>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
-<?php
+ <?php
 
-use Illuminate\Support\Facades\Route;
+ use App\Http\Controllers\BaseController;
+ use Illuminate\Support\Facades\Route;
+ use App\Http\Controllers\AdminPanel\BaseController as AdminBaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +14,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/animegirl',function(){
+    return 'gandırıldın';
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+ Route::get('/',[BaseController::class,'test'])->name('test');
+
+ Route::get('/index',[BaseController::class,'index'])->name('test');
+
+Route::get('/Parameter/{id}/{Number}',[BaseController::class,'param'])->name('parameter');
+
+Route::post('/save',[BaseController::class,'save'])->name('save');
+
+Route::get('/iop',[BaseController::class,'iop'])->name('iop');
+
+ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+     return view('dashboard');
+ })->name('dashboard');
+
+
+// ------------------ Admin Panel Routes -------------------//
+ Route::get('/admin',[AdminBaseController::class,'admin'])->name('admin');
+ Route::get('/yonetici',[AdminBaseController::class,'admin'])->name('admin');
+ // ------------------ Admin Category Routes -------------------//
+ Route::get('/yonetici/category',[\App\Http\Controllers\AdminPanel\CategoryController::class,'index'])->name('admin_category');
+ Route::get('/yonetici/category/create',[\App\Http\Controllers\AdminPanel\CategoryController::class,'create'])->name('admin_category_create');
+ Route::post('/yonetici/category/store',[\App\Http\Controllers\AdminPanel\CategoryController::class,'store'])->name('admin_category_store');
+
