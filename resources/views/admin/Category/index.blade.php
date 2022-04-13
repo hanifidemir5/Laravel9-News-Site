9fb1196d540cb313bfde6d371b7d1d1fad8a1350
Hanifi Demir <1910213059@ogrenci.karabuk.edu.tr>
2022-04-14 00:51:13 +0300

    Video 11 Update

--- resources/views/admin/Category/index.blade.php
+++ resources/views/admin/Category/index.blade.php
@@ -12,99 +12,60 @@
                 <!-- ============================================================== -->
                 <!-- pageheader  -->
                 <!-- ============================================================== -->
-                <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <div class="page-header">
-                            <h2 class="pageheader-title">Admin Panel Category List </h2>
-                            <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
-                            <div class="page-breadcrumb">
-                                <nav aria-label="breadcrumb">
-                                </nav>
+                <section class="content-header">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <div class="page-header">
+                                <h2 class="pageheader-title">Category List </h2>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="/laravelAdmin">Home</a></li>
+                                    <li class="breadcrumb-item active">Blank Page</li>
+                                </ol>
                             </div>
                         </div>
                     </div>
-                </div>
+                </section>
                 <!-- ============================================================== -->
                 <!-- end pageheader  -->
                 <!-- ============================================================== -->
 
                         <!-- recent orders  -->
                         <!-- ============================================================== -->
-                        <div class="col-xl-9 col-lg-12 col-md-6 col-sm-12 col-12">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="card">
-                                <h5 class="card-header">Recent Orders</h5>
-                                <div class="card-body p-0">
+                                <h5 class="card-header">Basic Table</h5>
+                                <div class="card-body">
                                     <div class="table-responsive">
-                                        <table class="table">
+                                        <table class="table table-striped table-bordered first">
                                             <thead class="bg-light">
                                             <tr class="border-0">
-                                                <th class="border-0">#</th>
+                                                <th class="border-0">Id</th>
                                                 <th class="border-0">Image</th>
-                                                <th class="border-0">Product Name</th>
-                                                <th class="border-0">Product Id</th>
-                                                <th class="border-0">Quantity</th>
-                                                <th class="border-0">Price</th>
-                                                <th class="border-0">Order Time</th>
-                                                <th class="border-0">Customer</th>
+                                                <th class="border-0">Tıtle</th>
+                                                <th class="border-0">Keywords</th>
+                                                <th class="border-0">Description</th>
                                                 <th class="border-0">Status</th>
+                                                <th class="border-0" style="width: 40px">Edit</th>
+                                                <th class="border-0" style="width: 40px">Delete</th>
+                                                <th class="border-0" style="width: 40px">Show</th>
                                             </tr>
                                             </thead>
                                             <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="admin/assets/images/product-pic.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #1 </td>
-                                                <td>id000001 </td>
-                                                <td>20</td>
-                                                <td>$80.00</td>
-                                                <td>27-08-2018 01:22:12</td>
-                                                <td>Patricia J. King </td>
-                                                <td><span class="badge-dot badge-brand mr-1"></span>InTransit </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="admin/assets/images/product-pic-2.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #2 </td>
-                                                <td>id000002 </td>
-                                                <td>12</td>
-                                                <td>$180.00</td>
-                                                <td>25-08-2018 21:12:56</td>
-                                                <td>Rachel J. Wicker </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="admin/assets/images/product-pic-3.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #3 </td>
-                                                <td>id000003 </td>
-                                                <td>23</td>
-                                                <td>$820.00</td>
-                                                <td>24-08-2018 14:12:77</td>
-                                                <td>Michael K. Ledford </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                            <tr>
-                                                <td>4</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="admin/assets/images/product-pic-4.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #4 </td>
-                                                <td>id000004 </td>
-                                                <td>34</td>
-                                                <td>$340.00</td>
-                                                <td>23-08-2018 09:12:35</td>
-                                                <td>Michael K. Ledford </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                            <tr>
-                                                <td colspan="9"><a href="#" class="btn btn-outline-light float-right">View Details</a></td>
-                                            </tr>
+                                            @foreach($data as $rs)
+                                                <tr>
+                                                    <td>{{$rs->id}}</td>
+                                                    <td>{{$rs->image}}</td>
+                                                    <td>{{$rs->title}}</td>
+                                                    <td>{{$rs->keywords}}</td>
+                                                    <td>{{$rs->description}}</td>
+                                                    <td>{{$rs->status}}</td>
+                                                    <td><a href="laravelAdmin/category/edit{{$rs->ıd}}/" class="btn btn-success" >Edit</a></td>
+                                                    <td><a href="laravelAdmin/category/delete{{$rs->ıd}}/" class="btn btn-danger">Delete</a></td>
+                                                    <td><a href="laravelAdmin/category/show{{$rs->ıd}}/" class="btn btn-info">Show</a></td>
+                                                </tr>
+                                            @endforeach
                                             </tbody>
                                         </table>
                                     </div>
